57357f648e92b44f83a17cca99e67e20676fe60b
akdo <109563467+akdo3@users.noreply.github.com>
2024-11-17 15:26:59 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,47 +1,45 @@
-// sw.js
-
-// مرحلة التثبيت وتخزين الملفات في الكاش
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('my-cache').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/styles.css',
-                '/script.js',
-                '/icons/icon-192x192.png',
-                '/icons/icon-512x512.png'
-            ]);
-        })
-    );
+// تثبيت الـ Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png'
+      ]);
+    })
+  );
 });
 
-// التعامل مع طلبات البيانات أثناء الاتصال أو عدمه
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request).then(function(fetchResponse) {
-                return caches.open('my-cache').then(function(cache) {
-                    cache.put(event.request, fetchResponse.clone());
-                    return fetchResponse;
-                });
-            });
-        })
-    );
+// التعامل مع طلبات الشبكة
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).then((fetchResponse) => {
+        return caches.open('my-cache').then((cache) => {
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
+    })
+  );
 });
 
-// مرحلة التفعيل والتحديث
-self.addEventListener('activate', function(event) {
-    var cacheWhitelist = ['my-cache'];
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.map(function(cacheName) {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+// تحديث الكاش بعد التفعيل
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = ['my-cache'];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
         })
-    );
+      );
+    })
+  );
 });
